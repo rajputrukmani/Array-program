e806dd4c2c2734f7ea70065f11893bbb4e8e2ae5
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-17 17:17:36 +0530

    Create Array9.java

---
+++ Array9.java
@@ -0,0 +1,10 @@
+ 
+
+public class ArrayFirstElement {
+    public static void main(String[] args) {
+        int arr[]={3,5,6,7,8};
+        int b=arr[0];
+        System.out.println(b);
+    }
+    
+}
