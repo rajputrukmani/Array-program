16c54f4c4cc7fd5d022859d0e815878f935b5efc
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-19 15:24:53 +0530

    ReplaceThirdElement.java

---
+++ Array10.java
@@ -0,0 +1,11 @@
+ public class ReplaceThirdElement {
+    public static void main(String[] args) {
+        int arr[]={6,7,8,9,10};
+        arr[2]=22;
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+
+}
