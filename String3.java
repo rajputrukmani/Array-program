6fa8c01a8c080af9907d596950ec273ab4a45361
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-27 08:02:43 +0530

    Create String3.java

---
+++ String3.java
@@ -0,0 +1,11 @@
+public class _0string{
+    public static void main(String[]args)
+    {
+        String name= "hello";
+      for(int i =0;i<name.length();i++){
+            char ch = name.charAt(i);
+          System.out.println(name.charAt(i));
+      }
+  }
+    
+}
