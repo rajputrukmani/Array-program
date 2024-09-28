58cb1bd377760a74a26e28c245187a713284a577
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-28 20:02:40 +0530

    spacescharacter.java

---
+++ Array11.java
@@ -0,0 +1,15 @@
+ class Schar {
+    public static void main(String[] args) {
+        int x[] ={2,4,5,1,6};
+        int a=1;
+        for(int i=0;i<x.length;i++)
+    {
+        if(x[i]==a)
+        {
+            System.out.println(x[i]);
+        }
+       
+    }
+    }
+    
+}
