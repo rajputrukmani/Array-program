40fdeccedbe7baeeb301b7e7f116d21b13e5cffc
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-10-01 22:01:14 +0530

    Create ArrayLProgram1.java

---
+++ ArrayL1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Demo3
+{
+ public static void main(String[] args) {
+  ArrayList <String> l1 = new ArrayList <>();
+  l1.add("Java");
+  l1.add("Python");
+  l1.add("Html");
+  l1.add(" Css");
+
+  for(String name :l1)
+  {
+    System.out.println("22="+name);
+  }
+
+  }
+  
+}
